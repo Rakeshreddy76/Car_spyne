e62458ccca0a48c088227ce97ac632ef355b8aa5
Rakesh Reddy <129585994+Rakeshreddy76@users.noreply.github.com>
2024-11-19 01:26:51 +0530

    Update CarForm.js

--- frontend/src/pages/CarForm.js
+++ frontend/src/pages/CarForm.js
@@ -14,7 +14,7 @@ function CarForm() {
     if (id) {
       const fetchCar = async () => {
         try {
-          const res = await axios.get(`https://car-spynee.onrender.com/api/cars/${id}`, {
+          const res = await axios.get(`https://car-spynee.onrender.com`, {
             headers: { 'x-auth-token': localStorage.getItem('token') }
           });
           const { title, description, images, tags } = res.data;
@@ -40,11 +40,11 @@ function CarForm() {
     };
     try {
       if (id) {
-        await axios.put(`https://car-spynee.onrender.com/api/cars/${id}`, carData, {
+        await axios.put(`https://car-spynee.onrender.com`, carData, {
           headers: { 'x-auth-token': localStorage.getItem('token') }
         });
       } else {
-        await axios.post('https://car-spynee.onrender.com/api/cars', carData, {
+        await axios.post('https://car-spynee.onrender.com', carData, {
           headers: { 'x-auth-token': localStorage.getItem('token') }
         });
       }
